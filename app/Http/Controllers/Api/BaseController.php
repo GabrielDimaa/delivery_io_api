8855d0af2282ee709574e8244e6e28495b76c1c1
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2021-12-21 23:34:52 -0300

    Base Controller

---
+++ app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
+
+abstract class BaseController extends Controller
+{
+    protected function sendResponse($data): JsonResponse
+    {
+        $response = array(
+            'success' => true,
+            'data' => $data,
+        );
+
+        return response()->json($response);
+    }
+
+    protected function sendResponseError($error, $code = 404): JsonResponse
+    {
+        $response = array(
+            'success' => false,
+            'error' => $error,
+        );
+
+        return response()->json($response, $code);
+    }
+
+    protected function validator(array $data, array $rules, array $messages = [], array $customAttributes = [])
+    {
+        return Validator::make($data, $rules, $messages, $customAttributes);
+    }
+
+    abstract protected function rules();
+    abstract protected function messages();
+}
