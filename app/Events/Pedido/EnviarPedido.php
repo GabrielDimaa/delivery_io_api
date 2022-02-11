f578f62882fd1a165b66695cb3533033b59c143a
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2022-02-11 20:29:00 -0300

    Instalação dos pacotes para o servidor WebSocket

---
+++ app/Events/Pedido/EnviarPedido.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events\Pedido;
+
+use App\Models\Pedido;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class EnviarPedido
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
